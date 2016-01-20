b1496c5fdb1967ba9360f072acbd1438620e98a8
Tomas Talandis <tomas@talandis.lt>
2016-01-20 09:13:02 +0200

    text handler restored

--- config/handlers/text.php
+++ config/handlers/text.php
@@ -7,30 +7,6 @@ $config = [
             'name' => 'text',
             'title' => 'Text',
         ],
-
-
-        [
-            'class' => 'Talandis\Larams\Property\Select',
-            'name' => 'color',
-            'title' => 'Color',
-            'options' => [
-                0 => 'No',
-                1 => 'Yes',
-            ],
-        ],
-
-        [
-            'class' => 'Talandis\Larams\Property\Image',
-            'name' => 'image',
-            'title' => 'Image',
-            'format' => 'png',
-            'versions' => [
-                'default' => [
-                    'width' => 200,
-                    'height' => 100
-                ],
-            ]
-        ],
     ],
 ];
 
