0c4e5a6473ac4b4f7f7473b3afe4076f89627bd0
Tomas Talandis <tomas@talandis.lt>
2016-08-16 13:20:16 +0300

    structure item picker select control fix

--- resources/views/properties/struct_items.blade.php
+++ resources/views/properties/struct_items.blade.php
@@ -2,36 +2,34 @@
 
     @if ( $style == 'SELECT' )
         <select {{$multiple ? 'multiple="multiple"' : ''}} name="data[{{$name}}]{{$multiple ? '[]' : ''}}" class="form-control">
-            @endif
+    @endif
 
             @if ( !empty( $allowEmpty ) && empty( $multiple ) )
-                <div class="checkbox">
-                    <label>
-                        <input type="radio" name="data[{{$name}}]" value=""/> {{__("Not selected")}}
-                    </label>
-                </div>
+                @if ( $style == 'SELECT')
+                    <option value=""><?=__("Not selected");?></option>
+                @else
+                    <div class="checkbox">
+                        <label>
+                            <input type="radio" name="data[{{$name}}]" value=""/> {{__("Not selected")}}
+                        </label>
+                    </div>
+                @endif
             @endif
 
             @foreach ( $childs as $child )
-
                 @if ( $style == 'SELECT' )
-
                     <option {{$child->selected ? 'selected="selected"' : ''}} value="{{$child->id}}">{{$child->whitespaces}}{{$child->name}}</option>
-
                 @else
-
                     <div class="checkbox">
                         <label style="padding-left: {{($child->level - $childs[0]->level + 1) * 20}}px">
                             <input {{$child->hasChilds ? 'style="display: none;"' : ''}} {{$child->selected ? 'checked="checked"' : ''}} type="{{$multiple ? 'checkbox' : 'radio'}}" name="data[{{$name}}]{{$multiple ? '[]' : ''}}" value="{{$child->id}}"/>
                             {{$child->name}}
                         </label>
                     </div>
-
                 @endif
-
             @endforeach
 
-            @if ( $style == 'SELECT' )
+    @if ( $style == 'SELECT' )
         </select>
     @endif
 
