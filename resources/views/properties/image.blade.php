16cda70993ad4ede48e8a955c92852a1ced530a9
Tomas Talandis <tomas@talandis.lt>
2016-08-20 16:30:58 +0300

    translations

--- resources/views/properties/image.blade.php
+++ resources/views/properties/image.blade.php
@@ -9,10 +9,10 @@
     @if ( !empty( $versions ))
 
         @if ( count( $versions) == 1)
-            <br />Paveiksliuko dydis: <strong>{{$versions['default']['width']}}x{{$versions['default']['height']}}</strong><br />
+            <br />{{__("Image size")}}: <strong>{{$versions['default']['width']}}x{{$versions['default']['height']}}</strong><br />
         @else
 
-            <br />Paveiksliuko dydžiai:<br />
+            <br />{{__("Image sizes")}}:<br />
 
             @foreach ( $versions as $versionName => $version )
                 @if (!empty( $version['title'] )) {{$version['title']}}: @else {{$versionName}} @endif <strong>{{$version['width']}}x{{$version['height']}}</strong><br />
@@ -24,38 +24,3 @@
     <!-- target={{$name}} -->
     <a class="btn btn-xs btn-default" href="#" onclick="window.open( '{{url('admin/gallery/index/0/1/'.$name )}}', 'image-{{$name}}', 'width=900,height=600' ); return false;"> <span class="fa fa-file-o"></span> {{__("Choose image")}}</a>
 </div>
-
-{{--
-$ret_val = '
-
-			<div>
-
-
-
-			';
-
-            if ( !empty( $this->_config['versions'] )) {
-
-                if ( count( $this->_config['versions'] ) == 1 ) {
-
-                    $version = $this->_config['versions'][0];
-                    $ret_val .= '';
-                } else {
-
-
-                }
-
-            }
-
-			if ( isset( $this->_intern_data[$this->_config['name']]['thumb']['url'] ) ) {
-
-				$ret_val .= '<div class="checkbox"><label><input type="checkbox" name="'.$prefix.'['.$this->_config['name'].'_delete]" /> Ištrinti paveiksliuka</label></div>';
-
-			}
-
-			$ret_val .= '
-			</div>
-			';
-
-		return( $ret_val );
-		--}}
