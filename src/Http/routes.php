c925d7281b01bc0db82b926ff84e2f39435b5e8a
Tomas Talandis <tomas@talandis.lt>
2017-02-28 15:33:29 +0200

    more gallery routes fixes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -37,8 +37,8 @@ Route::group( ['prefix' => env('BASE_URL', ''), 'middleware' => 'web' ], functio
             Route::get('gallery', 'GalleryController@getIndex');
             Route::get('gallery/index/{itemId?}/{select?}/{target?}', 'GalleryController@getIndex');
             Route::post('gallery/save-folder/{itemId?}', 'GalleryController@postSaveFolder');
-            Route::get('gallery/delete/{parentId}/{itemId}/{id}', 'GalleryController@getDelete');
-            Route::any('gallery/upload/{itemId}', 'GalleryController@anyUpload');
+            Route::get('gallery/delete/{parentId}/{itemId}/{id?}', 'GalleryController@getDelete');
+            Route::any('gallery/upload/{itemId}/{id?}', 'GalleryController@anyUpload');
             Route::post('gallery/move', 'GalleryController@postMove');
 
             Route::get('types', 'TypeController@getIndex');
