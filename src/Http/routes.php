d4d54f2fa2dc91983849630016fee31bb6979eba
Tomas Talandis <tomas@talandis.lt>
2017-10-17 13:09:35 +0300

    gallery folders saving route fix

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -38,7 +38,7 @@ Route::group( ['prefix' => env('BASE_URL', ''), 'middleware' => 'web' ], functio
 
             Route::get('gallery', 'GalleryController@getIndex');
             Route::get('gallery/index/{itemId?}/{select?}/{target?}', 'GalleryController@getIndex');
-            Route::post('gallery/save-folder/{itemId?}/{id?}', 'GalleryController@postSaveFolder');
+            Route::post('gallery/save-folder/{itemId?}/{id?}/{target?}', 'GalleryController@postSaveFolder');
             Route::get('gallery/delete/{parentId}/{itemId}/{id?}', 'GalleryController@getDelete');
             Route::any('gallery/upload/{itemId}/{id?}', 'GalleryController@anyUpload');
             Route::post('gallery/move', 'GalleryController@postMove');
