33852bb9f103c8d142083452d0b65283c10b8fdf
Tomas Talandis <tomas@talandis.lt>
2019-02-14 16:16:30 +0200

    allow changing main container class

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -9,7 +9,10 @@
     @include('larams::admin.blocks.navbar')
 @endif
 
-<div class="container" @if (!empty( $hideNavigationBar )) style="padding-top: 0 !important;" @endif>
+<div
+        class="@if (!empty( $mainContainerClass )) {{$mainContainerClass}} @else container @endif"
+        @if (!empty( $hideNavigationBar )) style="padding-top: 0 !important;" @endif
+>
 
     {!! $content !!}
 
@@ -18,7 +21,7 @@
 <div class="loader" style="display: none;" id="loader">
     <div class="loader-backdrop">&nbsp;</div>
     <div class="loader-content">
-        <img src="{{asset('vendor/larams/img/admin/loading.gif')}}" />
+        <img src="{{asset('vendor/larams/img/admin/loading.gif')}}"/>
         <div>{{trans('admin.text.loading')}}</div>
     </div>
 </div>
