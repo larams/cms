ddab6f2822fc1be3f34f45b678a343a9bd5bf09f
Tomas Talandis <tomas@talandis.lt>
2019-03-11 18:47:31 +0200

    reduce number of queries required for translations collection

--- src/TranslationKeyword.php
+++ src/TranslationKeyword.php
@@ -9,6 +9,8 @@ class TranslationKeyword extends \Eloquent
 
     protected $fillable = ['keyword'];
 
+    protected $translations = [];
+
     public function values()
     {
         return $this->hasMany('Larams\Cms\TranslationValue', 'keyword_id');
@@ -23,27 +25,34 @@ class TranslationKeyword extends \Eloquent
 
     }
 
-    public function translations( $locale, $group, $namespace = null )
+    public function translations($locale, $group, $namespace = null)
     {
-        $language = StructureItem::getModel()->byTypeName('site_lang')->whereData('short_code', $locale )->first();
+        $cacheKey = 'translations_' . $locale;
 
-        if (empty( $language )) {
-            return [];
+        if (empty($this->translations[$locale])) {
+            $language = StructureItem::getModel()
+                ->byTypeName('site_lang')
+                ->whereData('short_code', $locale)
+                ->first();
+
+            if (empty($language)) {
+                return [];
+            }
+
+            $this->translations[$locale] = $this
+                ->leftJoin('translation_values', 'translation_keywords.id', '=', 'translation_values.keyword_id')
+                ->where('translation_values.language_id', '=', $language->id)
+                ->where('translation_values.value', '!=', '')
+                ->pluck('value', 'keyword');
         }
 
-        $translations = $this
-            ->leftJoin('translation_values', 'translation_keywords.id', '=', 'translation_values.keyword_id')
-            ->where('translation_values.language_id', '=', $language->id )
-            ->where('translation_values.value', '!=', '')
-            ->pluck('value', 'keyword');
-
         $output = [];
-        foreach ( $translations as $k => $v ) {
-            if ( $group != '*' && !preg_match('/^'. preg_quote($group). '/si', $k )) {
+        foreach ($this->translations[$locale] as $k => $v) {
+            if ($group != '*' && !preg_match('/^' . preg_quote($group) . '/si', $k)) {
                 continue;
             }
-            $k = str_replace( $group.'.', '', $k );
-            $output[ $k] = $v;
+            $k = str_replace($group . '.', '', $k);
+            $output[$k] = $v;
         }
 
         return $output;
