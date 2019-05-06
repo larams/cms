01dde94fa54bf5a1a4bca1f8bee65a981c54643c
Tomas Talandis <tomas@talandis.lt>
2019-05-06 16:10:07 +0300

    remove double escaping in text property

--- resources/views/properties/text.blade.php
+++ resources/views/properties/text.blade.php
@@ -2,6 +2,6 @@
     <input
             class="form-control {{$cssClass}}"
             name="data[{{$name}}]"
-            value="{{htmlspecialchars( property_exists( $item->data, $name ) ? $item->data->$name  : '', ENT_QUOTES, 'UTF-8')}}"
+            value="{{ property_exists( $item->data, $name ) ? $item->data->$name  : '' }}"
     />
 </div>
