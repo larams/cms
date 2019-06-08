a4d55a91192c67a832ba4efe9f3523b567b78d73
Tomas Talandis <tomas@talandis.lt>
2019-06-08 09:25:30 +0300

    check if user agent header exists

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -4,7 +4,7 @@ if (!function_exists('webp')) {
     function webp()
     {
         $acceptWebpHeader = !empty($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false;
-        $isChrome = strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false;
+        $isChrome = !empty($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false;
 
 //        ((is.chrome && agent.satisfies('>=23.0.0'))
 //        					||  (is.opera && agent.satisfies('>=12.1'))
