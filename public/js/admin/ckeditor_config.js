e7cd42ae5578bb5b92a5f3664b569d6c23610070
Tomas Talandis <tomas@talandis.lt>
2019-06-14 15:13:42 +0300

    always paste as plain text

--- public/js/admin/ckeditor_config.js
+++ public/js/admin/ckeditor_config.js
@@ -3,21 +3,24 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function (config) {
 
-        config.allowedContent = true;
-        config.toolbar = [
-            { name: 'document', items: [ 'Source' ] },
-            //{ name: 'clipboard', items: [ 'Undo', 'Redo' ] },
-            { name: 'basicstyles',  items: [ 'Bold', 'Italic', 'Underline' ] },
-            { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-            { name: 'links', items: [ 'Link', 'Unlink' ] },
-            { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule' ] },
-            { name: 'styles', items: [ 'Format' ] },
-            { name: 'colors', items: [ 'TextColor', 'BGColor' ] }
-            //{ name: 'tools', items: [ 'Maximize' ] }
-    	];
+    config.allowedContent = true;
+    config.forcePasteAsPlainText = true;
+    config.toolbar = [
+        {name: 'document', items: ['Source']},
+        //{ name: 'clipboard', items: [ 'Undo', 'Redo' ] },
+        {name: 'basicstyles', items: ['Bold', 'Italic', 'Underline']},
+        {
+            name: 'paragraph',
+            items: ['NumberedList', 'BulletedList', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+        },
+        {name: 'links', items: ['Link', 'Unlink']},
+        {name: 'insert', items: ['Image', 'Table', 'HorizontalRule']},
+        {name: 'styles', items: ['Format']},
+        {name: 'colors', items: ['TextColor', 'BGColor']}
+        //{ name: 'tools', items: [ 'Maximize' ] }
+    ];
 
     config.extraPlugins = 'justify';
 
