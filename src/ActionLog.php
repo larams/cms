da6b4cb58af2a43a6a43e1a11ca9389f20740200
Tomas Talandis <tomas@talandis.lt>
2019-08-09 13:04:17 +0300

    improve action log

--- src/ActionLog.php
+++ src/ActionLog.php
@@ -10,19 +10,29 @@ class ActionLog extends Model
 
     protected $table = 'actions_log';
 
-    public static function log($relatedId, $type, $message, $data = null)
+    public static function adminLog($relatedId, $type, $message, $data = null)
     {
         if (!config('larams.admin.log_admin_actions')) {
             return null;
         }
 
+        return static::log($relatedId, $type, $message, $data);
+    }
+
+    public static function log($relatedId, $type, $message, $data = null)
+    {
+        $userId = null;
+
+        if (!auth()->guest()) {
+            $userId = auth()->user()->id;
+        }
+
         return static::create([
-            'user_id' => auth()->user()->id,
+            'user_id' => $userId,
             'related_id' => $relatedId,
             'type' => $type,
             'message' => $message,
             'data' => !empty($data) ? json_encode($data) : null
         ]);
     }
-
 }
