83e13cc7c2681eef91e284661c58a1e18a67be54
Tomas Talandis <tomas@talandis.lt>
2020-05-07 13:15:04 +0300

    fix double text hint position

--- resources/views/properties/double_text.blade.php
+++ resources/views/properties/double_text.blade.php
@@ -1,35 +1,30 @@
-<div class="row">
-    @if ( !empty( $inputs ))
-
+@if ( !empty( $inputs ))
+    <div class="row">
         @foreach ( $inputs as $input )
-
             <div class="{{$input['class']}}">
-
                 @if (!empty( $input['prefix'] ) || !empty( $input['suffix']))
                     <div class="input-group">
-                @endif
+                        @endif
 
-                    @if (!empty( $input['prefix']))
-                        <span class="input-group-addon">{{$input['prefix']}}</span>
-                    @endif
+                        @if (!empty( $input['prefix']))
+                            <span class="input-group-addon">{{$input['prefix']}}</span>
+                        @endif
 
-                        <input class="form-control" name="data[{{$name}}][{{$input['name']}}]" value="@if (isset( $item->data->{$name}->{$input['name']} ) ){{$item->data->{$name}->{$input['name']} }}@endif" />
+                        <input class="form-control" name="data[{{$name}}][{{$input['name']}}]"
+                               value="@if (isset( $item->data->{$name}->{$input['name']} ) ){{$item->data->{$name}->{$input['name']} }}@endif"/>
 
-                    @if (!empty( $input['suffix']))
-                        <span class="input-group-addon">{{$input['suffix']}}</span>
-                    @endif
+                        @if (!empty( $input['suffix']))
+                            <span class="input-group-addon">{{$input['suffix']}}</span>
+                        @endif
 
-                @if (!empty( $input['prefix'] ) || !empty( $input['suffix']))
+                        @if (!empty( $input['prefix'] ) || !empty( $input['suffix']))
                     </div>
                 @endif
-
             </div>
-
         @endforeach
+    </div>
+@endif
 
-        @if (!empty( $hint ))
-            <p class="help-block">{{$hint}}</p>
-        @endif
-
-    @endif
-</div>
+@if (!empty( $hint ))
+    <p class="help-block">{{$hint}}</p>
+@endif
