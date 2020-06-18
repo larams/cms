a720efceff12fa7e71976e417860ae7495eb93a0
Tomas Talandis <tomas@talandis.lt>
2020-06-18 15:49:37 +0300

    add attribute for all additional ids

--- src/StructureType.php
+++ src/StructureType.php
@@ -32,27 +32,45 @@ class StructureType extends Model
     public function getHandlers()
     {
         $handlersPath = config_path('handlers');
-        $handlers = \File::files( $handlersPath );
+        $handlers = \File::files($handlersPath);
 
-        $preparedHandlersPath = realpath( __DIR__ . '/../config/handlers' );
-        $preparedHandlers = \File::files( $preparedHandlersPath );
+        $preparedHandlersPath = realpath(__DIR__ . '/../config/handlers');
+        $preparedHandlers = \File::files($preparedHandlersPath);
 
-        $handlers = array_merge( $preparedHandlers, $handlers );
+        $handlers = array_merge($preparedHandlers, $handlers);
 
-        foreach ( $handlers as &$handler ) {
+        foreach ($handlers as &$handler) {
 
-            $handlerName = str_replace( array( $handlersPath . '/', '.php', $preparedHandlersPath . '/' ), '', $handler );
+            $handlerName = str_replace(array($handlersPath . '/', '.php', $preparedHandlersPath . '/'), '', $handler);
 
             $handler = array(
                 'id' => $handlerName,
-                'title' => str_replace('_', ' ', ucfirst( $handlerName ) )
+                'title' => str_replace('_', ' ', ucfirst($handlerName))
             );
         }
 
-        usort( $handlers, function($a,$b) {
+        usort($handlers, function ($a, $b) {
             return $a['title'] >= $b['title'];
         });
 
         return $handlers;
+    }
+
+    public function getAdditionalsAttribute()
+    {
+        $items = $this->types()->get();
+        $result = [];
+        $i = 0;
+        while ($i <= 2) {
+            $result[$i] = [];
+            $i++;
+        }
+        foreach ($items as $item) {
+            if (!array_key_exists($item->pivot->additional, $result)) {
+                $result[$item->pivot->additional] = [];
+            }
+            $result[$item->pivot->additional][] = $item->pivot->rel_type_id;
+        }
+        return $result;
     }
 }
