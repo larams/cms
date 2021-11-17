3d87d2536410e689d811de89d6c8bc577b9ee8f6
Tomas Talandis <tomas@talandis.lt>
2021-11-17 16:34:54 +0200

    create function to generate webp_url

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -10,9 +10,35 @@ if (!function_exists('webp')) {
 //        					||  (is.opera && agent.satisfies('>=12.1'))
 //        					||  (is.android && agent.satisfies('>=4.0')))
 
-        return config('larams.enable_webp') && ( $acceptWebpHeader || $isChrome );
+        return config('larams.enable_webp') && ($acceptWebpHeader || $isChrome);
     }
 }
+
+if (!function_exists('webp_url')) {
+    function webp_url($url)
+    {
+        if (!webp()) {
+
+            if (strpos($url, '.webp') !== false) {
+                return str_replace('.webp', '.png', $url);
+            }
+
+            return $url;
+        }
+
+        if (strpos($url, '.jpg') !== false) {
+            return str_replace('.jpg', '.webp', $url);
+        }
+
+
+        if (strpos($url, '.png') !== false) {
+            return str_replace('.png', '.webp', $url);
+        }
+
+        return $url;
+    }
+}
+
 
 if (!function_exists('dim')) {
     function dim(&$var, $default = null)
