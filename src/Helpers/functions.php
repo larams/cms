c876f7e5af34eee428b5e0bf1bbe3f5464d1c041
Tomas Talandis <tomas@talandis.lt>
2021-11-17 16:46:24 +0200

    manually enable webp support

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -3,14 +3,14 @@
 if (!function_exists('webp')) {
     function webp()
     {
-        $acceptWebpHeader = !empty($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false;
-        $isChrome = !empty($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false;
+//        $acceptWebpHeader = !empty($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false;
+//        $isChrome = !empty($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false;
 
 //        ((is.chrome && agent.satisfies('>=23.0.0'))
 //        					||  (is.opera && agent.satisfies('>=12.1'))
 //        					||  (is.android && agent.satisfies('>=4.0')))
 
-        return config('larams.enable_webp') && ($acceptWebpHeader || $isChrome);
+        return config('larams.enable_webp'); // && ($acceptWebpHeader || $isChrome);
     }
 }
 
